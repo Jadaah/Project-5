d4022ed2621718a5a9d0d665ca43b2aa388f4e46
Jadaah <jadaah.wingfield@patelhs.org>
2021-02-25 18:15:07 +0000

    main method

--- Main.java
+++ Main.java
@@ -1,8 +1,23 @@
 class Main {
   public static void main(String[] args) {
+    Scanner scan = new Scanner();
 
-scanner scan = new scanner();
-System.out.println("Welcome to Sweet's candy store");
+    System.out.println("Welcome to Sweet's candy store");
+    System.out.println("How much are you willing to spend");
+
+    int wallet = scan.nextInt();
+
+    System.out.println(candy(wallet, candyPieces));
+
+  }
+
+  public static int candy(wallet,candyPieces){
+ {
+ int wallet = 10;
+ if(wallet > 2 );
+ wallet = wallet-2;
+ }else{
+
 
   }
 }
